0fca2dca096f58fdca7451f70f75a309d75f0e8b
arunbear <arunbear@cpan.org>
2012-02-12 19:35:19 +0000

    added general predicate matching

--- Sub-PredicateDispatch/lib/Sub/PredicateDispatch.pm
+++ Sub-PredicateDispatch/lib/Sub/PredicateDispatch.pm
@@ -5,8 +5,22 @@ sub new {
     my ($class, %arg) = @_;
 
     my $f = sub { 
+        if((caller)[0] eq __PACKAGE__) {
+            return \%arg;
+        }
+
         my $it = shift;
-        $arg{when}{ $arg{dispatch}->($it) }->($it);  
+        my $dispatch_val = $arg{dispatch}->($it);
+
+        my @when = @{ $arg{when} };
+        while(my ($pred, $action) = splice @when, 0, 2) {
+            my $matched = ref $pred eq 'CODE' 
+              ? $pred->($dispatch_val) 
+              : $pred eq $dispatch_val;
+            if($matched) {
+                return $action->($it);
+            }
+        }
     };
     return bless $f => $class;
 }
--- Sub-PredicateDispatch/t/01.use.t
+++ Sub-PredicateDispatch/t/01.use.t
@@ -1,16 +1,20 @@
 use strict;
 use warnings;
 
-use Test::More tests => 1;
-
+use Test::More tests => 2;
+use Math::Trig;
 use Sub::PredicateDispatch;
 
 my $area = Sub::PredicateDispatch->new(
     dispatch => sub { $_[0]->{shape} },
-    when => {
-        square => sub { shift->{side} ** 2 }
-    },
+    when => [
+        square => sub { shift->{side} ** 2 },
+        sub { $_[0] eq 'circle' } => sub { pi * shift->{radius} ** 2 }, 
+    ],
 );
 
 my $square = { shape => 'square', side => 2 };
 is($area->($square), 4);
+
+my $circle = { shape => 'circle', radius => 1 };
+is($area->($circle), pi);
